cba2f97c944d7ada17a9fafe4990ff0631e75a93
Rafael C. Nunes <rafaelnunes737@hotmail.com>
2014-12-31 15:28:38 -0300

    Errors on the Font class were fixed. But be aware that the class leaks memory trying to render some font onto the screen.

--- src/font.cpp
+++ src/font.cpp
@@ -1,31 +1,35 @@
 #include "font.hpp"
 #include "debug.hpp"
+#include "window.hpp"
 
 #include <cassert>
 
-Font::Font(SDL_Renderer *font_renderer, SDL_Rect *font_rect) : FontManager()
+Font::Font(SDL_Rect& font_rect_) : FontManager()
 {
-    assert(font_rect != nullptr);
+    assert(font_rect_ != nullptr);
+
+    font_rect       = font_rect_;
 
     m_pFSurface     = nullptr;
     m_pFTexture     = nullptr;
-    m_pFRect        = font_rect;
-    m_pFRenderer    = font_renderer;
+    m_pFRenderer    = Window::get_renderer();
 
     assert(m_pFRenderer != nullptr);
-    assert(m_pFRect != nullptr);
+    assert(font_rect != nullptr);
 }
 
-Font::~Font() {}
+Font::~Font()
+{
+}
 
 
 void Font::open_font(FontType font_type, int size)
 {
     if(!m_ttf_opened) {
-
+        Debug::logerr("Font system is not running, see the logs for any errors \
+                      found in the initialization of it.");
+        return;
     }
-    // name of the font depending on the FontType
-    std::string font_name;
 
     switch(font_type)
     {
@@ -44,11 +48,13 @@ void Font::open_font(FontType font_type, int size)
 
     m_pFont = TTF_OpenFont(font_name.c_str(), size);
 
-    if(m_pFont == nullptr) {
+    if(m_pFont == nullptr)
+    {
         Debug::logerr("Failed to load the font: ", font_name, " Due to: ",
                       TTF_GetError());
     }
-    else {
+    else
+    {
         Debug::log("Font ", font_name, " loaded successfully.");
     }
 
@@ -56,26 +62,63 @@ void Font::open_font(FontType font_type, int size)
 
 void Font::render_text_solid(std::string text)
 {
-    if(m_pFont == nullptr) {
+    if(m_pFont == nullptr)
+    {
         Debug::logerr("You haven't loaded a font to display it.");
         return;
     }
 
     m_pFSurface = TTF_RenderText_Solid(m_pFont, text.c_str(), m_color);
 
-    if(m_pFSurface == nullptr) {
+    if(m_pFSurface == nullptr)
+    {
+        Debug::logerr("Failed to render the font", font_name, "onto the \
+                      screen");
+        return;
+    }
+
+    m_pFTexture = SDL_CreateTextureFromSurface(m_pFRenderer, m_pFSurface);
+
+    if(m_pFTexture == nullptr)
+    {
+        Debug::logerr("A texture could not be created from the surface, \
+                      error: ", TTF_GetError());
+        return;
+    }
+
+    SDL_RenderCopy(m_pFRenderer, m_pFTexture, nullptr, &font_rect);
+
+    // Free memory allocated by the texture.
+    SDL_DestroyTexture(m_pFTexture);
+}
+
+void Font::render_text_shaded(std::string text)
+{
+    if(m_pFont == nullptr)
+    {
+        Debug::logerr("You haven't loaded a font to display it.");
+        return;
+    }
+
+    m_pFSurface = TTF_RenderText_Shaded(m_pFont, text.c_str(), m_color,
+                                        m_color_shaded);
+
+    if(m_pFSurface == nullptr)
+    {
         Debug::logerr("");
         return;
     }
 
     m_pFTexture = SDL_CreateTextureFromSurface(m_pFRenderer, m_pFSurface);
 
-    if(m_pFSurface == nullptr) {
-        Debug::logerr("");
+    if(m_pFTexture == nullptr)
+    {
+        Debug::logerr("A texture could not be created from the surface, \
+                      error: ", TTF_GetError());
         return;
     }
 
-    SDL_RenderCopy(m_pFRenderer, m_pFTexture, nullptr, m_pFRect);
+    SDL_RenderCopy(m_pFRenderer, m_pFTexture, nullptr, &font_rect);
 
     // Free memory allocated by the texture.
     SDL_DestroyTexture(m_pFTexture);
--- src/font.hpp
+++ src/font.hpp
@@ -13,15 +13,17 @@ class Font : protected FontManager
 private:
     // The rect which will contain information about where the font will be
     // rendered.
-    SDL_Rect* m_pFRect;
+    SDL_Rect m_pFRect;
     // Surface used to carry the result of TTF_RenderText...
     SDL_Surface* m_pFSurface;
     // The texture that will be printed on the screen.
     SDL_Texture* m_pFTexture;
     // The renderer used by the font.
     SDL_Renderer* m_pFRenderer;
+    //
+    std::string font_name;
 public:
-    Font(SDL_Renderer *font_renderer, SDL_Rect *font_rect);
+    Font(SDL_Renderer *font_renderer, SDL_Rect &font_rect);
     ~Font();
 
     /**
@@ -41,23 +43,31 @@ public:
      */
     void open_font(FontType font_type, int size);
 
+    /**
+     * @brief render_text_solid, function to render a solid text onto the game
+     * window.
+     * @param text the text which will be rendered on the game window.
+     */
     void render_text_solid(std::string text);
-    void render_text_solid(const char* text);
-
-    void render_text_shaded(std::string text);
-    void render_text_shaded(const char* text);
 
     /**
-     * @brief font_color
-     * @param r
-     * @param g
-     * @param b
-     * @param a
+     * @brief render_text_shaded
+     * @param text
+     */
+    void render_text_shaded(std::string text);
+
+    /**
+     * @brief font_color is a function to set the color of the font.
+     * @param r - red
+     * @param g - green
+     * @param b - blue
+     * @param a - alpha
      */
     void font_color(u_int8_t r, u_int8_t g, u_int8_t b, u_int8_t a);
     /**
-     * @brief font_color
-     * @param colors
+     * @brief font_color is a function to set the color of the font.
+     * @param colors is an array that contain information about the 3 colors,
+     * plus an alpha.
      */
     void font_color(u_int8_t colors[3]);
 

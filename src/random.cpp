430c872d92b69d180a0c8cf937424ae8594c9557
Rafael C. Nunes <rafaelnunes737@hotmail.com>
2015-01-10 17:09:54 -0300

    Random class finished.

--- src/random.cpp
+++ src/random.cpp
@@ -1,7 +1,5 @@
 #include "random.hpp"
 
-#include <random>
-
 Random::Random()
 {
 }
@@ -10,7 +8,19 @@ Random::~Random()
 {
 }
 
-float Random::get_rand_number(float a, float b)
+
+float Random::get_real(float a, float b)
 {
-    linear
+    std::mt19937 generator(m_random_device());
+    std::uniform_real_distribution<float> distribution(a, b);
+
+    return distribution(generator);
+}
+
+int Random::get_int(float a, float b)
+{
+    std::mt19937 generator(m_random_device());
+    std::uniform_int_distribution<> distribution(a, b);
+
+    return distribution(generator);
 }
--- src/random.hpp
+++ src/random.hpp
@@ -1,15 +1,29 @@
 #ifndef RANDOM_HPP
 #define RANDOM_HPP
 
+#include <random>
+
 class Random {
 private:
-
+    std::random_device m_random_device;
 public:
     Random();
     ~Random();
 
-    // returns a float random number between an interval of a to b.
-    static float get_rand_number(float a, float b);
+    /**
+     * @brief get_real returns a real number between "a" and "b"
+     * @param a the
+     * @param b
+     * @return
+     */
+    float get_real(float a, float b);
+    /**
+     * @brief get_int
+     * @param a
+     * @param b
+     * @return
+     */
+    int get_int(float a, float b);
 };
 
 #endif // RANDOM_HPP
